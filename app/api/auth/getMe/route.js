b1e7ab89c959fbac17082dd16cbeec554df867d5
miknivin <116654570+miknivin@users.noreply.github.com>
2025-03-01 11:57:13 +0530

    mongodb error resolved

--- app/api/auth/getMe/route.js
+++ app/api/auth/getMe/route.js
@@ -1,9 +1,11 @@
+import dbConnect from "@/lib/db/connection";
 import { isAuthenticatedUser } from "@/middlewares/auth";
 import { NextResponse } from "next/server";
 
 
 export async function GET(req) {
   try {
+    await dbConnect();
     const user = await isAuthenticatedUser(req);
     return NextResponse.json({ success: true, user }, { status: 200 });
   } catch (error) {
