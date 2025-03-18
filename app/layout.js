85c4108561431b10c266404030b12158425f22ee
miknivin <116654570+miknivin@users.noreply.github.com>
2025-03-18 18:54:42 +0530

    ms clarity added

--- app/layout.js
+++ app/layout.js
@@ -41,6 +41,17 @@ export default function RootLayout({ children }) {
             src="https://www.facebook.com/tr?id=9518901524895878&ev=PageView&noscript=1"
           />
         </noscript>
+
+        {/* clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+             (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qq5qu3wvx2");
+          `}
+        </Script>
       </head>
       <body className="preload-wrapper">
         <div className="preload preload-container" id="preloader">
